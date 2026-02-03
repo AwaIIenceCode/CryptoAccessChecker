e9626472234ebf722ca946c48b3d4861dcddae35
AwallenceCode <awallencecode@gmail.com>
2026-02-03 16:13:21 +0200

    The skeleton has been written and the npm packages have been installed

---
+++ CryptoAccessChecker.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt-sync");
+
+//the main method that delegates the entire program
+function main()
+{
+
+    while (true)
+    {
+        switch(userChoise)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                return;
+            default:
+                break;
+        }
+    }
+}
