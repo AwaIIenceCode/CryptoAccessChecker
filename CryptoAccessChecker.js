08f761d98feaf93575530c94ec86dac2f1f6190e
AwallenceCode <awallencecode@gmail.com>
2026-02-03 16:36:31 +0200

    Add initial functions for age check, deposit, KYC, and crypto actions; update main method with user prompts

--- CryptoAccessChecker.js
+++ CryptoAccessChecker.js
@@ -1,11 +1,32 @@
 const prompt = require("prompt-sync");
 
+// fun for check user age
+function checkAge()
+{}
+
+// 
+function checkDeposit()
+{}
+
+//
+function checkKYC()
+{}
+
+//
+function runCryptoAction()
+{}
+
 //the main method that delegates the entire program
 function main()
 {
+    console.log("Welcome the acces checker in cryptocurrency");
+
+    let userChoise;
 
     while (true)
     {
+        console.log("Press \"1\" for buy crypto\nPress \"2\" for sell crypto\nPress \"3\" for futures trades\nPress \"4\" for exit b");
+
         switch(userChoise)
         {
             case 1:
